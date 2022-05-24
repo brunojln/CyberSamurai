47f757ec8de3b177ef54b9308b4d95a4930320ac
NathanYuhei <67643939+NathanYuhei@users.noreply.github.com>
2022-05-24 13:03:37 -0300

    agr vai

--- Entidades/Characters/Character.h
+++ Entidades/Characters/Character.h
@@ -11,7 +11,7 @@ namespace Entities {
         //core
         int lifePoints;
         int exp; //para o player sera += exp dos inimigos eliminados
-
+        //ok
 
     public:
         Character();
