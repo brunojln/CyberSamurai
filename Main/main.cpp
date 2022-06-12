71eecc3809a2b0c06aa0273eaa160cc1ff9dcaf9
NathanYuhei <67643939+NathanYuhei@users.noreply.github.com>
2022-06-12 17:30:42 -0300

    Update main.cpp

--- Main/main.cpp
+++ Main/main.cpp
@@ -9,3 +9,21 @@ int main()
     return 0;
 }
 
+/*
+ Referências
+
+  [1].SFML 2.4 For Beginners - 12: Collision Detection (AABB).. [S. I]: Hilze Vonck, 2016. Son., color.
+  Disponível em: https://www.youtube.com/watch?v=l2iCYCLi6MU&t=786s&ab_channel=HilzeVonck. Acesso em: 25 maio 2022.
+
+  [2].[ C++ & SFML - Simple 2D Games ]. Suécia: Suraj Sharma, 2020. P&B.
+  Disponível em: https://www.youtube.com/playlist?list=PL6xSOsbVA1eb_QqMTTcql_3PdOiE928up. Acesso em: 16 maio 2022.
+
+  [3].GOMILA, Laurent. SFML Documentation.
+  Disponível em: https://www.sfml-dev.org/documentation/2.5.1/. Acesso em: 10 maio 2022.
+
+  [4].TUTORIAL Jogo SFML. Curitiba: Matheus Burda, 2021. Son., color.
+  Disponível em: https://www.youtube.com/watch?v=y0KQIyqmUq8&list=PLSPev71NbUEBIQlT2QCd-gN6l_mNVw1cJ&ab_channel=BurdaCanal. Acesso em: 10 maio 2022.
+
+  [5].SIMÃO, Jean M.. TÉCNICA DE PROGRAMAÇÃO.
+  Disponível em: https://pessoal.dainf.ct.utfpr.edu.br/jeansimao/Fundamentos2/Fundamentos2.htm. Acesso em: 01 maio 2022.
+*/
